af6813bd79eb350913cb06a7127cb0990e93ac4c
Brent Bowles <bbowles@sileotech.com>
2021-06-04 16:52:35 -0700

    Weather

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,6 @@
 //*API key//
-let apiKey = "d107326fbabd2ac9b6880e26ef4fe2bd";
+var APIKey="a0aca8a89948154a4182dcecc780b513";
+
 
 
 var today = new Date();
@@ -8,33 +9,52 @@ let mm = String(today.getMonth() + 1).padStart(2, '0');
 let yyyy = today.getFullYear();
 var today = mm + '/' + dd + '/' + yyyy;
 
-//*function linkActions() {
-    $(searchButtonId).click(function () {
-      var input = $(searchInputId).val();
-      //we parse the input as int to see if its valid
-      if (isValidUSZip(input)) {
-        presentZip(input);
-      } else {
-        presentZipError();
-      }
-    });
+
     //}
 
-function getWeather(zipcode) {
-    let queryUrl = 'https://api.openweathermap.org/data/2.5/weather?appidq&zip='+zipCode;
-    fetch (url)
+function getWeather(zipCode) {
+    var queryUrl = 'https://api.openweathermap.org/data/2.5/weather?='+ zipCode + "&APPID=" + APIKey;
+   
+   
+    $.ajax({
+        url:queryUrl,
+        method: "GET",
+    })
     .then (function (response){
         return response.json();
+        console.log(response);
     }
     )
     .then (function (response){
         callback (response);
     });
 }
-
+ //Display weather results
+function displayWeather(event){
+    event.preventDefault();
+    if(searchCity.val().trim()!==""){
+        city=searchCity.val().trim();
+        currentWeather(city);
+    }
+}
+//*function linkActions() {
+    $(document).ready(function() {
+        
+        $(searchButtonId).click(function () {
+          var input = $(searchInputId).val();
+         
+          //we parse the input as int to see if its valid
+          //if (isValidUSZip(input)) {
+           // presentZip(input);
+         // } else {
+           // presentZipError();
+         // }
+        });
+        });
 
 $(document).on("click", ".historyEntry", function() {
     console.log("clicked history item")
     let thisElement = $(this);
     getWeather(thisElement.text());
+    getWeather(zipCode)
 });
